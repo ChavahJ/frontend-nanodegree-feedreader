3c0e8524f39dd7eae9a50263aadbafa828cd0a10
Chavah <elynchav@gmail.com>
2016-09-21 21:44:31 +0300

    Finished second and third test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,28 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
+        /* This is our second test - it tests that each feed
+         * in the allFeeds object has a URL defined
          * and that the URL is not empty.
          */
+        it('have defined URLs', function() {
+            allFeeds.forEach(function( feed ) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
+        /* This is our third test - it tests that each feed
+         * in the allFeeds object has a name defined
          * and that the name is not empty.
          */
+         it('have defined names', function() {
+             allFeeds.forEach(function( feed ) {
+                 expect(feed.name).toBeDefined();
+                 expect(feed.name.length).not.toBe(0);
+             });
+         });
+
     });
 
 
